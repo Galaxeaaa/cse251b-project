ff55ea5dcbeb2834e140e9ac46c4d38956c7b9f9
Cheng Wang <galaxea1111@gmail.com>
2023-05-23 21:57:27 -0700

    Update encoder_decoder.py.

--- dataloader.py
+++ dataloader.py
@@ -10,9 +10,10 @@ from glob import glob
 class ADataset(Dataset):
     """Dataset class for Argoverse"""
 
-    def __init__(self, path_list, left, right, transform=None):
+    def __init__(self, path_data, path_list, left, right, transform=None):
         super(ADataset, self).__init__()
 
+        self.path_data = path_data
         self.pkl_list = path_list
         self.l = left
         self.r = right
@@ -23,7 +24,7 @@ class ADataset(Dataset):
 
     def __getitem__(self, idx):
         pkl_path = self.pkl_list[self.l + idx]
-        with open(pkl_path, "rb") as f:
+        with open(self.path_data + pkl_path, "rb") as f:
             data = pickle.load(f)
 
         if self.transform:
@@ -35,27 +36,50 @@ class ADataset(Dataset):
 def my_collate(batch):
     """collate lists of samples into batches, create [ batch_sz x agent_sz x seq_len x feature]"""
     inp = [numpy.dstack([scene["p_in"], scene["v_in"]]) for scene in batch]
+    inp = numpy.array(inp)
     out = [numpy.dstack([scene["p_out"], scene["v_out"]]) for scene in batch]
+    out = numpy.array(out)
     inp = torch.LongTensor(inp)
     out = torch.LongTensor(out)
     return [inp, out]
 
 
-def loadData(path, city_index_path, batch_size = 4, split=0.9, cutoff=None):
+def collate_normalize(batch):
+    """collate lists of samples into batches, create [ batch_sz x agent_sz x seq_len x feature]"""
+    """!!!UNFINISHED!!! DONT USE THIS FUNCTION!!!"""
+    inp = [numpy.dstack([scene["p_in"], scene["v_in"]]) for scene in batch]
+    # normalize p_in in each scene
+    for i in range(len(inp)):
+        mean_p = numpy.mean(inp[i][:, :, 0], axis=0)
+        inp[i][:, :, 0] = inp[i][:, :, 0] - mean_p
+    inp = numpy.array(inp)
+    out = [numpy.dstack([scene["p_out"], scene["v_out"]]) for scene in batch]
+    out = numpy.array(out)
+    inp = torch.LongTensor(inp)
+    out = torch.LongTensor(out)
+    inp = inp / 300
+
+
+def loadData(path, city_index_path, batch_size=4, split=0.9, cutoff=None, normalize=False):
     # split train and valid data
-    # load st most cutoff sample (only when get city data)
+    # load at most cutoff sample (only when get city data)
 
     if not os.path.exists(path):
         raise Exception("Wrong Path:" + path)
 
-    if os.path.exists(city_index_path + "MIA.pkl") and os.path.exists(
-        city_index_path + "PIT.pkl"
-    ):
-        print("Read City File")
-        with open(city_index_path + "MIA.pkl", "rb") as file:
-            MIA_list = pickle.load(file)[:int(cutoff/2)]
-        with open(city_index_path + "PIT.pkl", "rb") as file:
-            PIT_list = pickle.load(file)[:int(cutoff/2)]
+    mia_path = city_index_path + "MIA.pkl"
+    pit_path = city_index_path + "PIT.pkl"
+
+    if os.path.exists(mia_path) and os.path.exists(pit_path):
+        print("Reading city index file...")
+        with open(mia_path, "rb") as file:
+            MIA_list = pickle.load(file)
+            if cutoff is not None:
+                MIA_list = MIA_list[: int(cutoff)]
+        with open(pit_path, "rb") as file:
+            PIT_list = pickle.load(file)
+            if cutoff is not None:
+                PIT_list = PIT_list[: int(cutoff)]
     else:
         print("Get City File")
         pkl_list = glob(os.path.join(path, "*"))[:cutoff]
@@ -75,45 +99,43 @@ def loadData(path, city_index_path, batch_size = 4, split=0.9, cutoff=None):
         MIA_list.sort()
         PIT_list.sort()
 
-        with open(city_index_path + "MIA.pkl", "wb") as file:
+        with open(mia_path, "wb") as file:
             pickle.dump(MIA_list, file)
-        with open(city_index_path + "PIT.pkl", "wb") as file:
+        with open(mia_path, "wb") as file:
             pickle.dump(PIT_list, file)
 
     MIA_S = int(len(MIA_list) * split)
     PIT_S = int(len(PIT_list) * split)
 
-    MIA_train_dataset = ADataset(MIA_list, 0, MIA_S)
-    MIA_valid_dataset = ADataset(MIA_list, MIA_S, len(MIA_list))
-    PIT_train_dataset = ADataset(PIT_list, 0, PIT_S)
-    PIT_valid_dataset = ADataset(PIT_list, PIT_S, len(PIT_list))
-
-    batch_sz = batch_size
+    MIA_train_dataset = ADataset(path, MIA_list, 0, MIA_S)
+    MIA_valid_dataset = ADataset(path, MIA_list, MIA_S, len(MIA_list))
+    PIT_train_dataset = ADataset(path, PIT_list, 0, PIT_S)
+    PIT_valid_dataset = ADataset(path, PIT_list, PIT_S, len(PIT_list))
 
     MIA_train_loader = DataLoader(
         MIA_train_dataset,
-        batch_size=batch_sz,
+        batch_size=batch_size,
         shuffle=False,
         collate_fn=my_collate,
         num_workers=0,
     )
     PIT_train_loader = DataLoader(
         PIT_train_dataset,
-        batch_size=batch_sz,
+        batch_size=batch_size,
         shuffle=False,
         collate_fn=my_collate,
         num_workers=0,
     )
     MIA_valid_loader = DataLoader(
         MIA_valid_dataset,
-        batch_size=batch_sz,
+        batch_size=batch_size,
         shuffle=False,
         collate_fn=my_collate,
         num_workers=0,
     )
     PIT_valid_loader = DataLoader(
         PIT_valid_dataset,
-        batch_size=batch_sz,
+        batch_size=batch_size,
         shuffle=False,
         collate_fn=my_collate,
         num_workers=0,
@@ -129,6 +151,3 @@ def loadData(path, city_index_path, batch_size = 4, split=0.9, cutoff=None):
         MIA_valid_dataset,
         PIT_valid_dataset,
     )
-
-
-# MIA_train_loader,PIT_train_loader,MIA_valid_loader,PIT_valid_loader,MIA_train_dataset,PIT_train_dataset,MIA_valid_dataset,PIT_valid_dataset = dataloader.loadData("C:\\Users\\zxk\\Desktop\\251B\\class-proj\\ucsd-cse-251b-class-competition\\train\\train","C:\\Users\\zxk\\Desktop\\251B\\class-proj\\ucsd-cse-251b-class-competition\\",split=0.9,cutoff=None)
--- encoder_decoder.py
+++ encoder_decoder.py
@@ -1,2 +1,173 @@
-from dataloader import dataloader
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import os, os.path
+import tqdm
+import matplotlib.pyplot as plt
 
+import dataloader
+
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=1):
+        super(EncoderDecoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.encoder = nn.LSTMCell(input_size, hidden_size)
+        self.attention = nn.MultiheadAttention(hidden_size, num_heads=1)
+        self.decoder = nn.LSTMCell(hidden_size, hidden_size)
+        self.dropout = nn.Dropout(p=0.2)
+        self.linear = nn.Linear(hidden_size, output_size)
+
+    def forward(
+        self, input, hidden_encoder, hidden_decoder, cell_encoder, cell_decoder
+    ):
+        for i in range(input.size(0)):
+            hidden_encoder, cell_encoder = self.encoder(
+                input[i], (hidden_encoder, cell_encoder)
+            )
+        hidden_decoder = hidden_encoder
+        output = [hidden_decoder]
+        for i in range(29):
+            hidden_decoder, cell_decoder = self.decoder(
+                hidden_decoder, (hidden_decoder, cell_decoder)
+            )
+            output.append(hidden_decoder)
+
+        output = torch.stack(output, dim=0)
+        # output = self.dropout(output)
+        output = self.linear(output)
+        return output, hidden_encoder, hidden_decoder, cell_encoder, cell_decoder
+
+    def initHidden(self, batch_size):
+        return torch.zeros(batch_size, self.hidden_size)
+
+    def initCell(self, batch_size):
+        return torch.zeros(batch_size, self.hidden_size)
+
+
+def train(
+    model,
+    dataloader,
+    criterion,
+    optimizer,
+    num_epochs=10,
+    print_every=10,
+    plot_every=1,
+):
+    model.train()
+    all_losses = []
+    print("Start Training")
+    progress_bar = tqdm.tqdm(range(num_epochs), ncols=100)
+    total_loss = 0
+    i = 0
+    for epoch in progress_bar:
+        for input, target in dataloader:
+            # print(input.size(), target.size())
+            input = (
+                input.permute(2, 0, 1, 3)
+                .reshape(input.size(2), input.size(0), -1)
+                .float()
+                .to(device)
+            )
+            target = (
+                target.permute(2, 0, 1, 3)
+                .reshape(target.size(2), target.size(0), -1)
+                .float()
+                .to(device)
+            )
+            batch_size = input.size(1)
+            hidden_encoder = model.initHidden(batch_size).to(device)
+            cell_encoder = model.initCell(batch_size).to(device)
+            hidden_decoder = model.initHidden(batch_size).to(device)
+            cell_decoder = model.initCell(batch_size).to(device)
+            optimizer.zero_grad()
+            output, hidden_encoder, hidden_decoder, cell_encoder, cell_decoder = model(
+                input, hidden_encoder, hidden_decoder, cell_encoder, cell_decoder
+            )
+            loss = criterion(output, target)
+            loss.backward()
+            total_loss += loss.item()
+            optimizer.step()
+            if i % print_every == 0:
+                progress_bar.set_description(
+                    "Epoch: %d, Loss: %.4f\t" % (epoch + 1, loss.item())
+                )
+            i += 1
+        all_losses.append(loss.item())
+
+    return all_losses
+
+
+def evaluate(model, dataloader, criterion):
+    model.eval()
+    current_loss = 0
+    with torch.no_grad():
+        for i, (input, target) in enumerate(dataloader):
+            input = (
+                input.permute(2, 0, 1, 3)
+                .reshape(input.size(2), input.size(0), -1)
+                .float()
+                .to(device)
+            )
+            target = (
+                target.permute(2, 0, 1, 3)
+                .reshape(target.size(2), target.size(0), -1)
+                .float()
+                .to(device)
+            )
+            batch_size = input.size(1)
+            hidden_encoder = model.initHidden(batch_size).to(device)
+            cell_encoder = model.initCell(batch_size).to(device)
+            hidden_decoder = model.initHidden(batch_size).to(device)
+            cell_decoder = model.initCell(batch_size).to(device)
+
+            output, hidden_encoder, hidden_decoder, cell_encoder, cell_decoder = model(
+                input, hidden_encoder, hidden_decoder, cell_encoder, cell_decoder
+            )
+            loss = criterion(output, target)
+            current_loss += loss.item()
+    return current_loss / len(dataloader)
+
+
+data_path = "./data/"
+city_index_path = "./"
+
+(
+    MIA_train_loader,
+    PIT_train_loader,
+    MIA_valid_loader,
+    PIT_valid_loader,
+    MIA_train_dataset,
+    PIT_train_dataset,
+    MIA_valid_dataset,
+    PIT_valid_dataset,
+) = dataloader.loadData(data_path, city_index_path, batch_size=16, cutoff=200)
+
+# print(next(iter(MIA_valid_loader))[0].size())
+# print(MIA_valid_loader[0].keys())
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+model_path = "./model/encoder_decoder.pt"
+model_path = ""
+if model_path and os.path.exists(model_path):
+    print(f"Loading model from {model_path}...")
+    model = EncoderDecoder(input_size=240, hidden_size=200, output_size=240).to(device)
+    model.load_state_dict(torch.load(model_path))
+else:
+    model = EncoderDecoder(input_size=240, hidden_size=200, output_size=240).to(device)
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+MIA_train_losses = train(model, MIA_train_loader, criterion, optimizer, num_epochs=1000)
+plt.plot(MIA_train_losses)
+plt.show()
+
+save_path = "./model/encoder_decoder.pt"
+torch.save(model.state_dict(), save_path)
+
+MIA_valid_loss = evaluate(model, MIA_valid_loader, criterion)
+print(f"MIA validation Loss: {MIA_valid_loss:.4f}")
